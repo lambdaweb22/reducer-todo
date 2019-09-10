e28a1928f2dc2f8c5ddacfd78ed51617606f07f4
Kenna <kennaalastair@gmail.com>
2019-09-10 14:31:55 -0700

    addTodo function working

---
+++ reducer-todo-app/src/App.js
@@ -0,0 +1,24 @@
+import React, { useReducer } from 'react';
+import './App.css';
+
+import { initialState, reducer } from './reducers/index';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log('state', state);
+
+  const addTodo = item => {
+    dispatch({ type: "ADD_TODO", payload: item });
+  };
+
+  return (
+    <div className="App">
+      <TodoForm addTodo={addTodo} />
+      <TodoList todoArray={state.todoArray} />
+    </div>
+  );
+}
+
+export default App;
---
+++ reducer-todo-app/src/components/TodoList.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const TodoList = ({ todoArray, toggleTodo }) => {
+    return (
+        <div>
+            {todoArray.map(todo => (
+                <div key={todo.id}
+                    onClick={() => toggleTodo(todo.id)}
+                    className={todo.completed ? "completed" : ""}>
+                    {todo.item}
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default TodoList;
